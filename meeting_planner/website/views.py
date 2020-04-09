732c31396e854d3f5afb9945559adb2ea2b39bc5
ernest1027 <ernestwong1027@gmail.com>
2020-04-09 17:34:43 -0400

    Initial commit

---
+++ meeting_planner/website/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+# Create your views here.
+from meetings.models import Meeting
+
+
+def welcome(request):
+    return render(request, "website/welcome.html", {"meetings": Meeting.objects.all()})
+
+def date(request):
+    return HttpResponse("The date is " + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("hi")
